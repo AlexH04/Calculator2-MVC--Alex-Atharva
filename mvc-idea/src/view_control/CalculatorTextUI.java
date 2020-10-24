dca0148d2446f590220564e4cb6cfd05409725bf
Alex Hu <56702257+AlexH04@users.noreply.github.com>
2020-10-24 12:40:07 -0700

    Debugged

--- mvc-idea/src/view_control/CalculatorTextUI.java
+++ mvc-idea/src/view_control/CalculatorTextUI.java
@@ -1,48 +1,53 @@
 package view_control;
+import util.Math;
+
 import java.util.Scanner;
 
 public class CalculatorTextUI {
     public static boolean valid = true;
-    public static String operator = null;
-    static boolean calculateAgain = false;
-    public static String input;
+    public static String operator = "null";
+    public static String input = null;
     static Scanner user = new Scanner(System.in);
 
     public static void main(String[] args) {
-        do {
-            askNumber();
-        } while (!calculateAgain); //keeps repeating until user decides to end program
+        operator = "null";
+        askNumber();
     }
 
     public static void askNumber() {
         do {
             System.out.println("Please enter a number between 0 and 9");
             input = user.nextLine();
+            valid = true;
             try {
-                int testInt = Integer.parseInt(input); //checks if user input was a number
+                double testInt = Double.parseDouble(input); //checks if user input was a number
             } catch (NumberFormatException nfe) {
                 valid = false;    //what to do if user input was not a string
             }
 
-            if (valid) { //if the user entered a number
+            if (valid && operator.equals("null")) { //if the user entered a number and this is the first number entered
                 askOperator(); //proceed to prompt for operator
             }
 
-            if (!operator.equals(null)) { //check if this is the second number entered -> part that calculates the answer
+            if (valid && !operator.equals("null")) { //check if this is the second number entered -> part that calculates the answer
                 CalcModel.saveValueOfArg2(Double.parseDouble(input)); //sets CalculatorUI.Arg2 to input via model
-                CalcModel.calculateAnswer(); //uses same functions in model as the jframe ui; so each model supports multiple uis
+                CalcModel.calculateAnswer(); //calculates answer and stores it in CalcModel.calAnswer variable
                 //this is the point of mvc
                 //even the variables used are the same: the savevalueofarg functions from model save the user input from both jframe and text into model
-                //print answer
+                System.out.println(CalcModel.calcAnswer); //uses same functions in model as the jframe ui; so each model supports multiple uis
                 boolean repeatValid = false;
                 do {
-                    //ask user if they want to calculate again (ask them to answer y or n)
-                    //if input is y
-                    calculateAgain = true; //will repeat the entire program in main()
-                    repeatValid = true;
-                    //else if input is n
-                    calculateAgain = false; //ends the program
-                    repeatValid = true;
+                    System.out.println("Would you like to calculate again? (y/n)");
+                    input = user.nextLine();
+                    if (input.toLowerCase().equals("y")) {
+                        repeatValid = true;
+                        main(null); //repeats the entire program
+                    } else if (input.toLowerCase().equals("n")) {
+                        repeatValid = true;
+                        System.exit(0); //ends the program
+                    } else {
+                        System.out.println("Please enter y/n:");
+                    }
                 } while (!repeatValid); //in case user enters something other than y or n: will keep prompting to try again
             }
         } while (!valid); //keeps asking user to enter a number until a valid number is entered
@@ -52,11 +57,27 @@ public class CalculatorTextUI {
         do {
             System.out.println("Would you like to +, -, *, or / ?");
             valid = false;
-            String operator = null;
             operator = user.nextLine();
-            if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")) {
+
+            if (operator.equals("+")) {
                 CalcModel.saveValueOfArg1(Double.parseDouble(input)); //sets CalculatorUI.Arg1 to input via model
+                CalcModel.saveValueOfMathOp(Math.OPERATOR.PLUS);
                 //input from previous value entered; passes data to the model that works regardless of JFrame or text ui as they both use the functions found in model; just the variable names are different in the different ui's
+                askNumber();
+                valid = true;
+            } else if (operator.equals("-")) {
+                CalcModel.saveValueOfArg1(Double.parseDouble(input)); //sets CalculatorUI.Arg1 to input via model
+                CalcModel.saveValueOfMathOp(Math.OPERATOR.MINUS);
+                askNumber();
+                valid = true;
+            } else if (operator.equals("*")) {
+                CalcModel.saveValueOfArg1(Double.parseDouble(input)); //sets CalculatorUI.Arg1 to input via model
+                CalcModel.saveValueOfMathOp(Math.OPERATOR.MULTIPLY);
+                askNumber();
+                valid = true;
+            } else if (operator.equals("/")) {
+                CalcModel.saveValueOfArg1(Double.parseDouble(input)); //sets CalculatorUI.Arg1 to input via model
+                CalcModel.saveValueOfMathOp(Math.OPERATOR.DIVIDE);
                 askNumber();
                 valid = true;
             }
